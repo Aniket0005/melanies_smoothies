3b29db371941204ccbaeb506e8efa99604dfad7f
Aniket Pabrekar <105861422+Aniket0005@users.noreply.github.com>
2024-11-14 15:40:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.session import Session
+from snowflake.snowpark.session import session
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -15,9 +15,9 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be: ', name_on_order)
 
 if session is None:
-    get_active_session = Session.builder.configs(snowflake_connection).create()
+    session = Session.builder.configs(snowflake_connection).create()
     
-session = get_active_session()
+##session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
